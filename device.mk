26f17bc392b48ae74fb6cde4e82c0d062b4353db
Carlo Dee <carlodee.official@proton.me>
2024-02-28 15:31:44 +0800

    m22: Added fastboot/d hal packages.
    Signed-off-by: Carlo Dee <carlodee.official@proton.me>

--- device.mk
+++ device.mk
@@ -12,3 +12,8 @@ PRODUCT_SHIPPING_API_LEVEL := 30
 
 # Dynamic Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# Fastboot/d Hal
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd
