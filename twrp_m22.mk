8743539bb0bdf44c7fc347ffc7a864f7b22e9a79
Carlo Dee <carlodee.official@proton.me>
2024-02-28 15:30:38 +0800

    m22: Cleanup device information that not necessary.
    Signed-off-by: Carlo Dee <carlodee.official@proton.me>

--- twrp_m22.mk
+++ twrp_m22.mk
@@ -18,12 +18,5 @@ $(call inherit-product, device/samsung/m22/device.mk)
 PRODUCT_DEVICE := m22
 PRODUCT_NAME := twrp_m22
 PRODUCT_BRAND := samsung
-PRODUCT_MODEL := SM-M225FV
+PRODUCT_MODEL := Samsung Galaxy M22
 PRODUCT_MANUFACTURER := samsung
-
-PRODUCT_GMS_CLIENTID_BASE := android-samsung-ss
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="m22jvxx-user 12 SP1A.210812.016 M225FVXXU8DWH4 release-keys"
-
-BUILD_FINGERPRINT := samsung/m22jvxx/m22:12/SP1A.210812.016/M225FVXXU8DWH4:user/release-keys
